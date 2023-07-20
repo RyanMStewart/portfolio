6bf9c0baf450acdb377ad39d2ef50079892ed38d
RyanMStewart <ryan86stewart@gmail.com>
2023-07-20 21:04:47 +0330

    add aniamtion to Skills section

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -1,20 +1,80 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
 import { GLOBAL_CONST } from "@/constance";
 
 const Skills = () => {
+  const borderRef = useRef(null);
+  const skillsRef = useRef(null);
+  const circleRef = useRef(null);
+  const captionRef = useRef(null);
+  const containerRef = useRef(null);
   const { skillsList } = GLOBAL_CONST;
 
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        scrub: 1.2,
+        start: "top center",
+        end: "bottom center",
+        trigger: containerRef.current,
+      },
+    });
+
+    tl.to(captionRef.current, {
+      y: 0,
+      duration: 2,
+      ease: "power1.out",
+    })
+      .to(
+        circleRef.current,
+        {
+          opacity: 1,
+          duration: 3,
+          ease: "power1.out",
+        },
+        "<"
+      )
+      .to(
+        Array.from(skillsRef.current.children).map((el) => el.children),
+        {
+          y: 1,
+          duration: 3,
+          stagger: 0.4,
+          ease: "power1.out",
+        },
+        "<"
+      )
+      .to(borderRef.current, {
+        scaleX: 1,
+        duration: 3,
+        ease: "power1.out",
+      });
+
+    return () => {
+      tl.kill();
+    };
+  }, []);
+
   return (
-    <section className="relative py-[10em] text-center">
-      <div className="section-circle right-[-27em] top-[-20em] text-[1.3em]" />
-      <span className="font-caption text-[1.25em] mb-[2em] block">Skills</span>
-      <ul className="grid grid-cols-3 gap-[3em]">
+    <section ref={containerRef} className="relative py-[10em] text-center">
+      <div
+        ref={circleRef}
+        className="section-circle right-[-27em] top-[-20em] text-[1.3em] opacity-0"
+      />
+      <div className="overflow-hidden text-[1.25em] mb-[2em]">
+        <span ref={captionRef} className="font-caption  block translate-y-full">
+          Skills
+        </span>
+      </div>
+      <ul ref={skillsRef} className="grid grid-cols-3 gap-[3em]">
         {skillsList.map(({ id, title }) => (
-          <li key={id} className="text-[3.25em]">
-            <h2>{title}</h2>
+          <li key={id} className="text-[3.25em] overflow-hidden">
+            <h2 className="translate-y-full">{title}</h2>
           </li>
         ))}
       </ul>
-      <div className="section-border" />
+      <div ref={borderRef} className="section-border scale-x-0" />
     </section>
   );
 };
